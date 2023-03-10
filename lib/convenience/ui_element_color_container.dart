a3060ebfd6d6e07f20e21a840c94c64842a93866
Adrian Samoticha <adrian@samoticha.de>
2023-03-10 19:02:18 +0100

    add UI element color container

---
+++ lib/convenience/ui_element_color_container.dart
@@ -0,0 +1,443 @@
+import 'dart:ui';
+
+import 'package:appkit_ui_element_colors/appkit_ui_element_colors.dart';
+import 'package:appkit_ui_element_colors/appkit_ui_element_colors_platform_interface.dart';
+import 'package:appkit_ui_element_colors/macos/ns_appearance_name.dart';
+
+import 'package:equatable/equatable.dart';
+
+/// A convenience class that contains all
+/// [UI Element Colors](https://developer.apple.com/documentation/appkit/nscolor/ui_element_colors).
+class UiElementColorContainer extends Equatable {
+  /// Creates a [UiElementColorContainer] with the specified values.
+  const UiElementColorContainer._({
+    required this.labelColor,
+    required this.secondaryLabelColor,
+    required this.tertiaryLabelColor,
+    required this.quaternaryLabelColor,
+    required this.textColor,
+    required this.placeholderTextColor,
+    required this.selectedTextColor,
+    required this.textBackgroundColor,
+    required this.selectedTextBackgroundColor,
+    required this.keyboardFocusIndicatorColor,
+    required this.unemphasizedSelectedTextColor,
+    required this.unemphasizedSelectedTextBackgroundColor,
+    required this.linkColor,
+    required this.separatorColor,
+    required this.selectedContentBackgroundColor,
+    required this.unemphasizedSelectedContentBackgroundColor,
+    required this.selectedMenuItemTextColor,
+    required this.gridColor,
+    required this.headerTextColor,
+    required this.alternatingContentBackgroundColors0,
+    required this.alternatingContentBackgroundColors1,
+    required this.controlAccentColor,
+    required this.controlColor,
+    required this.controlBackgroundColor,
+    required this.controlTextColor,
+    required this.disabledControlTextColor,
+    required this.currentControlTint,
+    required this.selectedControlColor,
+    required this.selectedControlTextColor,
+    required this.alternateSelectedControlTextColor,
+    required this.scrubberTexturedBackground,
+    required this.windowBackgroundColor,
+    required this.windowFrameTextColor,
+    required this.underPageBackgroundColor,
+    required this.findHighlightColor,
+    required this.highlightColor,
+    required this.shadowColor,
+  });
+
+  /// Fetches the UI Element Colors from the Swift side and creates a
+  /// [UiElementColorContainer] out of them.
+  static Future<UiElementColorContainer> generate(
+      NSAppearanceName appearance) async {
+    return UiElementColorContainer._(
+      labelColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.labelColor, appearance),
+      secondaryLabelColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.secondaryLabelColor, appearance),
+      tertiaryLabelColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.tertiaryLabelColor, appearance),
+      quaternaryLabelColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.quaternaryLabelColor, appearance),
+      textColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.textColor, appearance),
+      placeholderTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.placeholderTextColor, appearance),
+      selectedTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.selectedTextColor, appearance),
+      textBackgroundColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.textBackgroundColor, appearance),
+      selectedTextBackgroundColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.selectedTextBackgroundColor, appearance),
+      keyboardFocusIndicatorColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.keyboardFocusIndicatorColor, appearance),
+      unemphasizedSelectedTextColor: await AppkitUiElementColorsPlatform
+          .instance
+          .getColor(UiElementColor.unemphasizedSelectedTextColor, appearance),
+      unemphasizedSelectedTextBackgroundColor:
+          await AppkitUiElementColorsPlatform.instance.getColor(
+              UiElementColor.unemphasizedSelectedTextBackgroundColor,
+              appearance),
+      linkColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.linkColor, appearance),
+      separatorColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.separatorColor, appearance),
+      selectedContentBackgroundColor: await AppkitUiElementColorsPlatform
+          .instance
+          .getColor(UiElementColor.selectedContentBackgroundColor, appearance),
+      unemphasizedSelectedContentBackgroundColor:
+          await AppkitUiElementColorsPlatform.instance.getColor(
+              UiElementColor.unemphasizedSelectedContentBackgroundColor,
+              appearance),
+      selectedMenuItemTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.selectedMenuItemTextColor, appearance),
+      gridColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.gridColor, appearance),
+      headerTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.headerTextColor, appearance),
+      alternatingContentBackgroundColors0:
+          await AppkitUiElementColorsPlatform.instance.getColor(
+              UiElementColor.alternatingContentBackgroundColors0, appearance),
+      alternatingContentBackgroundColors1:
+          await AppkitUiElementColorsPlatform.instance.getColor(
+              UiElementColor.alternatingContentBackgroundColors1, appearance),
+      controlAccentColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.controlAccentColor, appearance),
+      controlColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.controlColor, appearance),
+      controlBackgroundColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.controlBackgroundColor, appearance),
+      controlTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.controlTextColor, appearance),
+      disabledControlTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.disabledControlTextColor, appearance),
+      currentControlTint: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.currentControlTint, appearance),
+      selectedControlColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.selectedControlColor, appearance),
+      selectedControlTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.selectedControlTextColor, appearance),
+      alternateSelectedControlTextColor:
+          await AppkitUiElementColorsPlatform.instance.getColor(
+              UiElementColor.alternateSelectedControlTextColor, appearance),
+      scrubberTexturedBackground: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.scrubberTexturedBackground, appearance),
+      windowBackgroundColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.windowBackgroundColor, appearance),
+      windowFrameTextColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.windowFrameTextColor, appearance),
+      underPageBackgroundColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.underPageBackgroundColor, appearance),
+      findHighlightColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.findHighlightColor, appearance),
+      highlightColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.highlightColor, appearance),
+      shadowColor: await AppkitUiElementColorsPlatform.instance
+          .getColor(UiElementColor.shadowColor, appearance),
+    );
+  }
+
+  // === Label Colors ===
+
+  /// The primary color to use for text labels.
+  final Color labelColor;
+
+  /// The secondary color to use for text labels.
+  final Color secondaryLabelColor;
+
+  /// The tertiary color to use for text labels.
+  final Color tertiaryLabelColor;
+
+  /// The quaternary color to use for text labels and separators.
+  final Color quaternaryLabelColor;
+
+  // === Text Colors ===
+
+  /// The color to use for text.
+  final Color textColor;
+
+  /// The color to use for placeholder text in controls or text views.
+  final Color placeholderTextColor;
+
+  /// The color to use for selected text.
+  final Color selectedTextColor;
+
+  /// The color to use for the background area behind text.
+  final Color textBackgroundColor;
+
+  /// The color to use for the background of selected text.
+  final Color selectedTextBackgroundColor;
+
+  /// The color to use for the keyboard focus ring around controls.
+  final Color keyboardFocusIndicatorColor;
+
+  /// The color to use for selected text in an unemphasized context.
+  final Color unemphasizedSelectedTextColor;
+
+  /// The color to use for the text background in an unemphasized context.
+  final Color unemphasizedSelectedTextBackgroundColor;
+
+  // === Content Colors ===
+
+  /// The color to use for links.
+  final Color linkColor;
+
+  /// The color to use for separators between different sections of content.
+  final Color separatorColor;
+
+  /// The color to use for the background of selected and emphasized content.
+  final Color selectedContentBackgroundColor;
+
+  /// The color to use for selected and unemphasized content.
+  final Color unemphasizedSelectedContentBackgroundColor;
+
+  // === Menu Colors ===
+
+  /// The color to use for the text in menu items.
+  final Color selectedMenuItemTextColor;
+
+  // === Table Colors ===
+
+  /// The color to use for the optional gridlines, such as those in a table
+  /// view.
+  final Color gridColor;
+
+  /// The color to use for text in header cells in table views and outline
+  /// views.
+  final Color headerTextColor;
+
+  /// The colors to use for alternating content, typically found in table views
+  /// and collection views.
+  final Color alternatingContentBackgroundColors0;
+
+  /// The colors to use for alternating content, typically found in table views
+  /// and collection views.
+  final Color alternatingContentBackgroundColors1;
+
+  // === Control Colors ===
+
+  /// The user's current accent color preference.
+  final Color controlAccentColor;
+
+  /// The color to use for the flat surfaces of a control.
+  final Color controlColor;
+
+  /// The color to use for the background of large controls, such as scroll
+  /// views or table views.
+  final Color controlBackgroundColor;
+
+  /// The color to use for text on enabled controls.
+  final Color controlTextColor;
+
+  /// The color to use for text on disabled controls.
+  final Color disabledControlTextColor;
+
+  /// The current system control tint color.
+  final Color currentControlTint;
+
+  /// The color to use for the face of a selected control—that is, a control
+  /// that has been clicked or is being dragged.
+  final Color selectedControlColor;
+
+  /// The color to use for text in a selected control—that is, a control being
+  /// clicked or dragged.
+  final Color selectedControlTextColor;
+
+  /// The color to use for text in a selected control.
+  final Color alternateSelectedControlTextColor;
+
+  /// The patterned color to use for the background of a scrubber control.
+  final Color scrubberTexturedBackground;
+
+  // === Window Colors ===
+
+  /// The color to use for the window background.
+  final Color windowBackgroundColor;
+
+  /// The color to use for text in a window's frame.
+  final Color windowFrameTextColor;
+
+  /// The color to use in the area beneath your window's views.
+  final Color underPageBackgroundColor;
+
+  // === Highlights and Shadows ===
+
+  /// The highlight color to use for the bubble that shows inline search result
+  /// values.
+  final Color findHighlightColor;
+
+  /// The color to use as a virtual light source on the screen.
+  final Color highlightColor;
+
+  /// The color to use for virtual shadows cast by raised objects on the screen.
+  final Color shadowColor;
+
+  Color getColorFromUiElementColor(UiElementColor uiElementColor) {
+    switch (uiElementColor) {
+      case UiElementColor.labelColor:
+        return labelColor;
+
+      case UiElementColor.secondaryLabelColor:
+        return secondaryLabelColor;
+
+      case UiElementColor.tertiaryLabelColor:
+        return tertiaryLabelColor;
+
+      case UiElementColor.quaternaryLabelColor:
+        return quaternaryLabelColor;
+
+      case UiElementColor.textColor:
+        return textColor;
+
+      case UiElementColor.placeholderTextColor:
+        return placeholderTextColor;
+
+      case UiElementColor.selectedTextColor:
+        return selectedTextColor;
+
+      case UiElementColor.textBackgroundColor:
+        return textBackgroundColor;
+
+      case UiElementColor.selectedTextBackgroundColor:
+        return selectedTextBackgroundColor;
+
+      case UiElementColor.keyboardFocusIndicatorColor:
+        return keyboardFocusIndicatorColor;
+
+      case UiElementColor.unemphasizedSelectedTextColor:
+        return unemphasizedSelectedTextColor;
+
+      case UiElementColor.unemphasizedSelectedTextBackgroundColor:
+        return unemphasizedSelectedTextBackgroundColor;
+
+      case UiElementColor.linkColor:
+        return linkColor;
+
+      case UiElementColor.separatorColor:
+        return separatorColor;
+
+      case UiElementColor.selectedContentBackgroundColor:
+        return selectedContentBackgroundColor;
+
+      case UiElementColor.unemphasizedSelectedContentBackgroundColor:
+        return unemphasizedSelectedContentBackgroundColor;
+
+      case UiElementColor.selectedMenuItemTextColor:
+        return selectedMenuItemTextColor;
+
+      case UiElementColor.gridColor:
+        return gridColor;
+
+      case UiElementColor.headerTextColor:
+        return headerTextColor;
+
+      case UiElementColor.alternatingContentBackgroundColors0:
+        return alternatingContentBackgroundColors0;
+
+      case UiElementColor.alternatingContentBackgroundColors1:
+        return alternatingContentBackgroundColors1;
+
+      case UiElementColor.controlAccentColor:
+        return controlAccentColor;
+
+      case UiElementColor.controlColor:
+        return controlColor;
+
+      case UiElementColor.controlBackgroundColor:
+        return controlBackgroundColor;
+
+      case UiElementColor.controlTextColor:
+        return controlTextColor;
+
+      case UiElementColor.disabledControlTextColor:
+        return disabledControlTextColor;
+
+      case UiElementColor.currentControlTint:
+        return currentControlTint;
+
+      case UiElementColor.selectedControlColor:
+        return selectedControlColor;
+
+      case UiElementColor.selectedControlTextColor:
+        return selectedControlTextColor;
+
+      case UiElementColor.alternateSelectedControlTextColor:
+        return alternateSelectedControlTextColor;
+
+      case UiElementColor.scrubberTexturedBackground:
+        return scrubberTexturedBackground;
+
+      case UiElementColor.windowBackgroundColor:
+        return windowBackgroundColor;
+
+      case UiElementColor.windowFrameTextColor:
+        return windowFrameTextColor;
+
+      case UiElementColor.underPageBackgroundColor:
+        return underPageBackgroundColor;
+
+      case UiElementColor.findHighlightColor:
+        return findHighlightColor;
+
+      case UiElementColor.highlightColor:
+        return highlightColor;
+
+      case UiElementColor.shadowColor:
+        return shadowColor;
+    }
+  }
+
+  @override
+  String toString() {
+    final buffer = StringBuffer();
+    final strings = UiElementColor.values
+        .map((e) => '$e: ${getColorFromUiElementColor(e)}');
+    buffer.writeAll(strings, ', ');
+    return buffer.toString();
+  }
+
+  @override
+  List<Object?> get props => [
+        labelColor,
+        secondaryLabelColor,
+        tertiaryLabelColor,
+        quaternaryLabelColor,
+        textColor,
+        placeholderTextColor,
+        selectedTextColor,
+        textBackgroundColor,
+        selectedTextBackgroundColor,
+        keyboardFocusIndicatorColor,
+        unemphasizedSelectedTextColor,
+        unemphasizedSelectedTextBackgroundColor,
+        linkColor,
+        separatorColor,
+        selectedContentBackgroundColor,
+        unemphasizedSelectedContentBackgroundColor,
+        selectedMenuItemTextColor,
+        gridColor,
+        headerTextColor,
+        alternatingContentBackgroundColors0,
+        alternatingContentBackgroundColors1,
+        controlAccentColor,
+        controlColor,
+        controlBackgroundColor,
+        controlTextColor,
+        disabledControlTextColor,
+        currentControlTint,
+        selectedControlColor,
+        selectedControlTextColor,
+        alternateSelectedControlTextColor,
+        scrubberTexturedBackground,
+        windowBackgroundColor,
+        windowFrameTextColor,
+        underPageBackgroundColor,
+        findHighlightColor,
+        highlightColor,
+        shadowColor,
+      ];
+}
