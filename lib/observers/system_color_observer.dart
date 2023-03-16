078b04fd0577153aac9cc5b758bc8fcc1b188d3a
Adrian Samoticha <adrian@samoticha.de>
2023-03-16 14:16:57 +0100

    document system color observer

--- lib/observers/system_color_observer.dart
+++ lib/observers/system_color_observer.dart
@@ -2,14 +2,22 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-/// TODO: document this
+/// A class that observes changes to the system colors.
+///
+/// The class sends a null value through its [stream] whenever a
+/// `NSColor.systemColorsDidChangeNotification` is received on the Swift side.
 class SystemColorObserver {
+  /// This [SystemColorObserver]’s method channel.
   final _channel =
       const MethodChannel('appkit_ui_element_colors/system_colors_did_change');
+
+  /// The [StreamController] for the [stream].
   final _streamController = StreamController<void>.broadcast();
 
+  /// A [Stream] that can used to listen for system color changes.
   Stream<void> get stream => _streamController.stream;
 
+  /// Creates a [SystemColorObserver].
   SystemColorObserver() {
     _channel.setMethodCallHandler((call) async {
       switch (call.method) {
